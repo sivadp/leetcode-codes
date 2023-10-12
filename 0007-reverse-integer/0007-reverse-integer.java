18da4fe2340b06f6c274f05bee3675abcd6a998e
sivadp <90540454+sivadp@users.noreply.github.com>
2023-10-12 20:51:08 +0530

    Time: 1 ms (97.67%), Space: 40.2 MB (13.46%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,20 +1,17 @@
 class Solution {
     public int reverse(int x) {
-        String str=String.valueOf(x);
-        StringBuilder strbul=new StringBuilder();
-        if(str.charAt(0)=='-'){
-        strbul=new StringBuilder(str.substring(1)).reverse();
-        strbul.insert(0,"-");
-        }
-        else{
-        strbul=new StringBuilder(str).reverse();
-        }
-        try{
-        int revInt=Integer.parseInt(strbul.toString());
-        return revInt;
-        }
-        catch(Exception e){
+        int result=0;
+        while(x!=0){
+            int digit=x%10;
+        if(result>Integer.MAX_VALUE/10||result==Integer.MAX_VALUE&&digit>7){
             return 0;
         }
+        if(result<Integer.MIN_VALUE/10||result==Integer.MIN_VALUE&&digit<8){
+            return 0;
+        }
+        result=result*10+digit;
+        x/=10;
+        }
+        return result;
     }
 }

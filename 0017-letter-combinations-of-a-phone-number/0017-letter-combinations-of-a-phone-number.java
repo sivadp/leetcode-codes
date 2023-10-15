3ab27bc427121ca28c6b589b01c25cd73a7b5324
sivadp <90540454+sivadp@users.noreply.github.com>
2023-10-15 09:05:23 +0530

    Time: 2 ms (47.79%), Space: 41.1 MB (52.23%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,42 +1,34 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class Solution {
+class Solution {
     public List<String> letterCombinations(String digits) {
-        if (digits.equals("")) {
+        if(digits.equals("")){
             return Arrays.asList();
         }
-
-        Map<Character, List<String>> mapper = new HashMap<>();
-        mapper.put('2', Arrays.asList("a", "b", "c"));
-        mapper.put('3', Arrays.asList("d", "e", "f"));
-        mapper.put('4', Arrays.asList("g", "h", "i"));
-        mapper.put('5', Arrays.asList("j", "k", "l"));
-        mapper.put('6', Arrays.asList("m", "n", "o"));
-        mapper.put('7', Arrays.asList("p", "q", "r", "s"));
-        mapper.put('8', Arrays.asList("t", "u", "v"));
-        mapper.put('9', Arrays.asList("w", "x", "y", "z"));
-
-        List<String> resstr = new ArrayList<>();
-
-        for (char digit : digits.toCharArray()) {
-            if (resstr.isEmpty()) {
-                resstr.addAll(mapper.get(digit));
-            } else {
-                List<String> curstr = new ArrayList<>();
-                List<String> lister = mapper.get(digit);
-
-                for (String str : resstr) {
-                    for (String suffix : lister) {
-                        curstr.add(str + suffix);
-                    }
-                }
-                resstr = curstr;
+        Map<String,List<String>> mapper=new HashMap<>();
+        mapper.put("2",Arrays.asList("a","b","c"));
+        mapper.put("3",Arrays.asList("d","e","f"));
+        mapper.put("4",Arrays.asList("g","h","i"));
+        mapper.put("5",Arrays.asList("j","k","l"));
+        mapper.put("6",Arrays.asList("m","n","o"));
+        mapper.put("7",Arrays.asList("p","q","r","s"));
+        mapper.put("8",Arrays.asList("t","u","v"));
+        mapper.put("9",Arrays.asList("w","x","y","z"));
+        String[] arrdigits=digits.split("");
+        List<String> resstr=new ArrayList<>();
+        for(int i=0;i<arrdigits.length;i++){
+            if(resstr.size()==0){
+                resstr.addAll(mapper.get(arrdigits[i]));
+                continue;
             }
-        }
+            List<String> curstr=new ArrayList<>();
+            List<String> lister=mapper.get(arrdigits[i]);
+            for(int j=0;j<resstr.size();j++){
+                for(int k=0;k<lister.size();k++){
+                    String str=resstr.get(j)+lister.get(k);
+                    curstr.add(str);
+                }
+            }
+            resstr=curstr;
+        } 
         return resstr;
     }
 }

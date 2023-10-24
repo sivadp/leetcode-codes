2bf2993541a5d43c04594aefb49b2b93eb1dd389
sivadp <90540454+sivadp@users.noreply.github.com>
2023-10-24 21:04:49 +0530

    Time: 550 ms (13.21%), Space: 43.8 MB (80.87%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -6,54 +6,32 @@ import java.util.Set;
 
 public class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Set<List<Integer>> resultSet = new HashSet<>(); 
-
-        Arrays.sort(nums); 
-        do {
-            List<Integer> numList = new ArrayList<>();
-            for (int num : nums) {
-                numList.add(num);
+         List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        find(nums, nums.length, 0, ans);
+        return ans;
+    }
+    public static void find(int[] arr, int n, int ind, List<List<Integer>> ans){
+        if(ind == n){
+            List<Integer> ds = new ArrayList<>();
+            for(int it : arr){
+                ds.add(it);
             }
-            resultSet.add(numList);
-
-        } while (nextPermutation(nums));
-
-        result.addAll(resultSet);
-        return result;
+            if(ans.contains(ds)){
+                return;
+            }
+            ans.add(ds);
+            return;
+        }
+        for(int i=ind; i<n;i++){
+            swap(arr, i, ind);
+            find(arr, n, ind+1, ans);
+            swap(arr, i, ind);
+        }
     }
-
-    private boolean nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-        if (i == -1) {
-            return false;
-        }
-
-        int j = nums.length - 1;
-        while (j > i && nums[j] <= nums[i]) {
-            j--;
-        }
-
-        swap(nums, i, j);
-        reverse(nums, i + 1);
-        return true;
-    }
-
-    private static void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    private static void reverse(int[] nums, int start) {
-        int end = nums.length - 1;
-        while (start < end) {
-            swap(nums, start, end);
-            start++;
-            end--;
-        }
+    public static void swap(int[] arr, int si, int ei){
+        int t = arr[si];
+        arr[si] = arr[ei];
+        arr[ei] = t;
     }
 }

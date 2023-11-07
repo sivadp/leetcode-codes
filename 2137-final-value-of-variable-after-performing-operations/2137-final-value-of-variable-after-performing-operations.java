df7a9047cdd3eab7818b64bb1cc9e3156c2b452b
sivadp <90540454+sivadp@users.noreply.github.com>
2023-11-07 21:12:26 +0530

    Time: 0 ms (100%), Space: 41.2 MB (85.4%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -2,12 +2,7 @@ class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int res=0;
         for(String x:operations){
-            if(x.equals("--X")||x.equals("X--")){
-                res-=1;
-            }
-            else{
-                res+=1;
-            }
+            res+=44-x.charAt(1);
         }
         return res;
     }

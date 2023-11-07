73e8d64f94c7c4a48695a70ae5adf514b51d86fe
sivadp <90540454+sivadp@users.noreply.github.com>
2023-11-07 12:09:13 +0530

    Time: 0 ms (100%), Space: 40.9 MB (21.54%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.java
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -1,19 +1,9 @@
 class Solution {
-        public static int numJewelsInStones(String jewels, String stones) {
-        // Create a HashMap to store the counts of each stone type
-        Map<Character, Integer> stoneCounts = new HashMap<>();
-
-        // Count occurrences of each stone type in stones
-        for (char stone : stones.toCharArray()) {
-            stoneCounts.put(stone, stoneCounts.getOrDefault(stone, 0) + 1);
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        for(int i=0;i<stones.length();i++){
+            if(jewels.indexOf(stones.charAt(i))!=-1)count++;
         }
-
-        // Sum up the counts of stones that are also jewels
-        int jewelCount = 0;
-        for (char jewel : jewels.toCharArray()) {
-            jewelCount += stoneCounts.getOrDefault(jewel, 0);
-        }
-
-        return jewelCount;
+        return count;
     }
 }

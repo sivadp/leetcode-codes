cfe4e13f77d522a518c67cbe068b5adacc957672
siva <90540454+sivadp@users.noreply.github.com>
2023-11-08 19:21:39 +0530

    Time: 13 ms (99.64%), Space: 45.7 MB (98.21%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.java
+++ 0817-design-hashmap/0817-design-hashmap.java
@@ -1,27 +1,73 @@
+
 class MyHashMap {
-    private int[] mapper;
+    private static final int CAPACITY = 10003;
+    private Entry[] table;
+
+    class Entry{
+        int key;
+        int value;
+        Entry next;
+        Entry(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
     public MyHashMap() {
-        mapper=new int[1000001];
-        Arrays.fill(mapper,-1);
+        table = new Entry[CAPACITY];
     }
     
     public void put(int key, int value) {
-        mapper[key]=value;
+        int index = hash(key);
+        Entry entry = table[index];
+
+        while(entry != null) {
+            if(entry.key == key) {
+                entry.value = value;
+                return;
+            }
+            entry = entry.next;
+        }
+
+        Entry newEntry = new Entry(key, value);
+        newEntry.next = table[index];
+        table[index] = newEntry;
     }
     
     public int get(int key) {
-        return mapper[key];
+        int index = hash(key);
+        Entry entry = table[index];
+
+        while(entry != null) {
+            if(entry.key == key) {
+                return entry.value;
+            }
+            entry = entry.next;
+        }
+
+        return -1;
     }
     
     public void remove(int key) {
-        mapper[key]=-1;
+        int index = hash(key);
+        Entry entry = table[index];
+        Entry prev = null;
+
+        while (entry != null) {
+            if (entry.key == key) {
+                if (prev == null) {
+                    table[index] = entry.next; // Update the bucket's head if the key is at the head.
+                } else {
+                    prev.next = entry.next; // Update the previous entry's next pointer.
+                }
+                return;
+            }
+            prev = entry;
+            entry = entry.next;
+        }
+    }
+
+    private int hash(int key) {
+        return key % CAPACITY; // A simple hash function using modulo.
     }
 }
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
- */

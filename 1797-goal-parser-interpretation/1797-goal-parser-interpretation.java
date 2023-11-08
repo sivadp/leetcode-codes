0fb840387d6fd8df4a7475885db936be5feb14a5
sivadp <90540454+sivadp@users.noreply.github.com>
2023-11-08 08:16:59 +0530

    Time: 0 ms (100%), Space: 40.4 MB (60.17%) - LeetHub

---
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder res=new StringBuilder();
+        for(int i=0;i<command.length();i++){
+            if(command.charAt(i)=='('){
+                if(command.charAt(i+1)==')'){
+                    res.append("o");
+                    i++;
+                }
+                else{
+                    res.append("al");
+                    i+=3;
+                }
+            }
+            else{
+                res.append(command.charAt(i));
+            }
+        }
+        return res.toString();
+    }
+}

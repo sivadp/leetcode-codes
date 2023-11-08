0f3dab36a5e7db36bdc38b2139a89ed8da8fde91
siva <90540454+sivadp@users.noreply.github.com>
2023-11-08 22:39:09 +0530

    Time: 3 ms (99.99%), Space: 56.8 MB (53.58%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,29 +1,43 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
+        // Edge-case : 
+        if(head==null && head.next==null){
             return true;
         }
 
-        Stack<Integer> stacker = new Stack<>();
+        // Step 1 --> find mid of list (i.e slow)
         ListNode slow = head;
         ListNode fast = head;
 
-        while (fast != null && fast.next != null) {
-            stacker.push(slow.val);
+        while(fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        // If the length of the linked list is odd, skip the middle element
-        if (fast != null) {
-            slow = slow.next;
+        // Step 2 --> reverse list from mid 
+        ListNode prev = null;
+        ListNode current = slow;
+
+        while(current!=null){
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
         }
 
-        while (slow != null) {
-            if (stacker.isEmpty() || stacker.pop() != slow.val) {
+        // put mid.next = null to divide list into two part (here, slow = mid)
+        slow.next = null;
+
+        // Step 3 --> Iterate over both the lists and check if the value of nodes are equal or not if not equal then it is not palindrome list
+        ListNode p1 = head;
+        ListNode p2 = prev;
+
+        while(p1!=null && p2!=null){
+            if(p1.val != p2.val){
                 return false;
             }
-            slow = slow.next;
+            p1 = p1.next;
+            p2 = p2.next;
         }
 
         return true;
